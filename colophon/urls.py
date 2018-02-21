631dd8b8a0d2b16f0fff5a7b435d28734f1e9086
James Quinlan <jamesquinlan@prometheus.local>
2018-02-20 21:15:21 -0500

    build nav

--- colophon/urls.py
+++ colophon/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from core.views.dashboard import DashboardView
 
 urlpatterns = [
-    path('', DashboardView.as_view()),
+    path('', DashboardView.as_view(), name='dashboard'),
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
 ]
