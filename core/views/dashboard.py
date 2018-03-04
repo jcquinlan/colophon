bd81a399c50ab1e5a0e2c6677851b4b38ae6a84e
jcquinlan <jcquinlan.dev@gmail.com>
2018-03-04 17:14:44 -0500

    Dashboard pagination

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -1,4 +1,5 @@
 from django.views import View
+from django.core.paginator import Paginator
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from core.forms.create_design_document_form import CreateDesignDocumentForm
@@ -9,7 +10,12 @@ class DashboardView(LoginRequiredMixin, View):
     template_name = 'core/dashboard.html'
 
     def get(self, request):
-        documents = DesignDocument.objects.all()
+        all_documents = DesignDocument.objects.all()
+        paginator = Paginator(all_documents, 10)
+
+        page = request.GET.get('page')
+
+        documents = paginator.get_page(page)
 
         context = {
             'documents': documents,
