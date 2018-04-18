87343d1b4356c0e27f9b1fb1633828d616e09d0c
jcquinlan <jcquinlan.dev@gmail.com>
2018-04-17 22:38:34 -0400

    Delete images when editing

--- core/forms/edit_design_document_form.py
+++ core/forms/edit_design_document_form.py
@@ -1,6 +1,6 @@
 import json
 from django import forms
-from core.models import DesignDocument
+from core.models import DesignDocument, DesignDocumentImage
 
 class EditDesignDocumentForm(forms.ModelForm):
     document_images = forms.CharField(widget=forms.HiddenInput(), required=False, label='')
@@ -23,12 +23,16 @@ class EditDesignDocumentForm(forms.ModelForm):
         # Mutably move the file urls from the cleaned data
         file_urls_string = data.pop('document_images')
         asset_url_string = data.pop('asset_url')
+        document_images_to_remove_string = data.pop('document_images_to_remove')
 
         # When editing a file, these fields will be empty, since the values of the inputs
         # need to be manually set, as they are hidden. This means that editing a design_document
         # does not duplicate the fule_urls or asset_url over and over.
         file_urls = json.loads(file_urls_string) if file_urls_string else []
         asset_url = json.loads(asset_url_string) if asset_url_string else None
+
+        document_images_to_remove = json.loads(document_images_to_remove_string) \
+            if document_images_to_remove_string else []
 
         # Assign the user that created/edited the document
         design_document.uploaded_by = request.user
@@ -56,6 +60,15 @@ class EditDesignDocumentForm(forms.ModelForm):
             design_document_image.save()
 
             design_document.images.add(design_document_image)
+
+        for url in document_images_to_remove:
+            design_document_images = DesignDocumentImage.objects.filter(
+                design_document=design_document,
+                image_url=url
+            )
+
+            if design_document_images:
+                design_document_images.delete()
 
         design_document.save()
 
